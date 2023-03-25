2f9e53106eeca1648a0233e2af7cd3a1d22a5067
strivitech <strivitech@gmail.com>
2023-03-25 17:09:48 +0200

    Add pages

---
+++ WorkshopSearch/WebApp/ClientApp/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Hero, Services, Contact } from '../components'
+const HomePage = () => {
+  return (
+    <main>
+      <Hero />
+      <Services />
+      <Contact />
+    </main>
+  )
+}
+
+export default HomePage
---
+++ WorkshopSearch/WebApp/ClientApp/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Error from './ErrorPage'
+export {
+  Home,
+  SingleProduct,
+  Error,
+  Products,
+}
