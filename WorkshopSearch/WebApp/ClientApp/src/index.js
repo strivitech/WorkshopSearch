2f08ebd933e26021e87e2ff297472d9013ff932e
strivitech <strivitech@gmail.com>
2023-03-25 17:11:08 +0200

    Add index.js

---
+++ WorkshopSearch/WebApp/ClientApp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <FilterProvider>
+        <ProductsProvider>
+            <App />
+        </ProductsProvider>
+    </FilterProvider>
+);
