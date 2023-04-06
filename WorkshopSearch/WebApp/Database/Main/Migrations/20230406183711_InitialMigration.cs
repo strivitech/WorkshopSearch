029940f5cfa0da4b32f847978bd0baf301a41347
strivitech <strivitech@gmail.com>
2023-04-06 21:47:28 +0300

    Create new migration

---
+++ WorkshopSearch/WebApp/Database/Main/Migrations/20230406183711_InitialMigration.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Main.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workshops",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ContactInformation_PhoneNumber = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    ContactInformation_Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ContactInformation_ContactLinks = table.Column<string>(type: "text", nullable: false),
+                    Constrains_MinAge = table.Column<int>(type: "integer", nullable: false),
+                    Constrains_MaxAge = table.Column<int>(type: "integer", nullable: false),
+                    Constrains_Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Address = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    ImageUris = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    Description = table.Column<string>(type: "character varying(10000)", maxLength: 10000, nullable: false),
+                    Owner = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workshops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DirectionWorkshop",
+                columns: table => new
+                {
+                    DirectionsId = table.Column<int>(type: "integer", nullable: false),
+                    WorkshopId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DirectionWorkshop", x => new { x.DirectionsId, x.WorkshopId });
+                    table.ForeignKey(
+                        name: "FK_DirectionWorkshop_Directions_DirectionsId",
+                        column: x => x.DirectionsId,
+                        principalTable: "Directions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DirectionWorkshop_Workshops_WorkshopId",
+                        column: x => x.WorkshopId,
+                        principalTable: "Workshops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirectionWorkshop_WorkshopId",
+                table: "DirectionWorkshop",
+                column: "WorkshopId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DirectionWorkshop");
+
+            migrationBuilder.DropTable(
+                name: "Directions");
+
+            migrationBuilder.DropTable(
+                name: "Workshops");
+        }
+    }
+}
