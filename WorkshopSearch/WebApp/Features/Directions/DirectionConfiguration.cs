7b3f6fa932b72ed25e52b35950dc56a600049a37
strivitech <strivitech@gmail.com>
2023-04-06 21:42:57 +0300

    Add configuration for Direction entity

---
+++ WorkshopSearch/WebApp/Features/Directions/DirectionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebApp.Features.Directions;
+
+public class DirectionConfiguration : IEntityTypeConfiguration<Direction>
+{
+    private const int NameMaxLength = 100;
+    
+    public void Configure(EntityTypeBuilder<Direction> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                x => x.Value, 
+                x => new DirectionId(x));
+
+        builder.Property(x => x.Name).HasMaxLength(NameMaxLength).IsRequired();
+    }
+}
