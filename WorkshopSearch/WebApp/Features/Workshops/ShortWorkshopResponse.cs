8bbbf9ede8ba278c043f3dac74f45827ce26a90d
strivitech <strivitech@gmail.com>
2023-04-07 18:29:29 +0300

    Add response models for workshops (full,short)

---
+++ WorkshopSearch/WebApp/Features/Workshops/ShortWorkshopResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Features.Workshops;
+
+public class ShortWorkshopResponse
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Owner { get; set; } = null!;
+    public int MinAge { get; set; }
+    public int MaxAge { get; set; }
+    public decimal Price { get; set; }
+    public string Address { get; set; } = null!;
+    public string CoverImageUri { get; set; } = null!;
+}
