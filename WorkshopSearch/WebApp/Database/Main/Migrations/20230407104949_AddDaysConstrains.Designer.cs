e3aec33ad5c36c918c52bc37cde1db59650ba72d
strivitech <strivitech@gmail.com>
2023-04-07 18:27:53 +0300

    Add new migrations

--- WorkshopSearch/WebApp/Database/Main/Migrations/20230407104949_AddDaysConstrains.Designer.cs
+++ WorkshopSearch/WebApp/Database/Main/Migrations/20230407104949_AddDaysConstrains.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApp.Database.Main;
@@ -11,9 +12,11 @@ using WebApp.Database.Main;
 namespace WebApp.Database.Main.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407104949_AddDaysConstrains")]
+    partial class AddDaysConstrains
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -61,11 +64,6 @@ namespace WebApp.Database.Main.Migrations
                         .IsRequired()
                         .HasMaxLength(200)
                         .HasColumnType("character varying(200)");
-
-                    b.Property<string>("CoverImageUri")
-                        .IsRequired()
-                        .HasMaxLength(1000)
-                        .HasColumnType("character varying(1000)");
 
                     b.Property<string>("Description")
                         .IsRequired()
---
+++ WorkshopSearch/WebApp/Database/Main/Migrations/20230407143857_AddCoverImageUri.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Main.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoverImageUri : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImageUri",
+                table: "Workshops",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverImageUri",
+                table: "Workshops");
+        }
+    }
+}
