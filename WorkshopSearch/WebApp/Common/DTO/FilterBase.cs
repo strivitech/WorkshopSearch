187207d18c6c0ebc4c5226aba4df2a2aa9585129
strivitech <strivitech@gmail.com>
2023-04-07 18:30:50 +0300

    Add mapper to short workshop response model

---
+++ WorkshopSearch/WebApp/Common/DTO/FilterBase.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Common.DTO;
+
+public class FilterBase
+{
+    public const int DefaultSize = 6;
+    public const int MaxSize = 100;
+    
+    private int _size = DefaultSize;
+
+    [Range(0, int.MaxValue)]
+    public int From { get; set; } = 0;
+    
+    public int Size
+    {
+        get => _size;
+
+        set
+        {
+            _size = value switch
+            {
+                < 0 => DefaultSize,
+                > MaxSize => MaxSize,
+                _ => value
+            };
+        }
+    }
+}
