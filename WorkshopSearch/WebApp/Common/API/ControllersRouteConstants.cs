c087563010674660dcc71ad23f37bc6af01407ee
strivitech <strivitech@gmail.com>
2023-04-11 11:22:37 +0300

    Add DefaultControllerActionRoute

--- WorkshopSearch/WebApp/Common/API/ControllersRouteConstants.cs
+++ WorkshopSearch/WebApp/Common/API/ControllersRouteConstants.cs
@@ -5,4 +5,5 @@ public static class ControllersRouteConstants
     public const string Api = "api/";
     public const string Controller = "[controller]";
     public const string DefaultRestRoute = Api + Controller;
+    public const string DefaultControllerActionRoute = DefaultRestRoute + "/[action]";
 }
