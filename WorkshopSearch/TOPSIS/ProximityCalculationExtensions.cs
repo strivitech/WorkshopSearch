2dcadd917b52be976177c326dab7c16975643827
strivitech <strivitech@gmail.com>
2023-04-11 10:51:54 +0300

    Make Topsis worker method returns relative closeness in descending order

--- WorkshopSearch/TOPSIS/ProximityCalculationExtensions.cs
+++ WorkshopSearch/TOPSIS/ProximityCalculationExtensions.cs
@@ -2,6 +2,18 @@
 
 internal static class ProximityCalculationExtensions
 {
+    public static List<IdentifiedRelativeCloseness<TIdentifier>> OrderByCalculatedProximityDesc<TIdentifier>(
+        this IEnumerable<IdentifiedDistanceScope<TIdentifier>> identifiedDistanceScope)
+    {
+        return identifiedDistanceScope.Select(distanceScope =>
+            new IdentifiedRelativeCloseness<TIdentifier>(distanceScope.Identifier,
+                distanceScope.DistanceToNegativeSolution /
+                (distanceScope.DistanceToPositiveSolution +
+                 distanceScope.DistanceToNegativeSolution)))
+            .OrderByDescending(relativeCloseness => relativeCloseness.RelativeCloseness)
+            .ToList();
+    }
+
     public static List<IdentifiedRelativeCloseness<TIdentifier>> CalculateProximity<TIdentifier>(
         this IEnumerable<IdentifiedDistanceScope<TIdentifier>> identifiedDistanceScope)
     {
