c0132f1ae6e0841a0509fa0c6296cc9fd11b1c58
strivitech <strivitech@gmail.com>
2023-04-12 11:29:14 +0300

    Add migration updating address for workshop entity

---
+++ WorkshopSearch/WebApp/Database/Main/Migrations/20230412081808_Update_Workshop_Address.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Main.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Workshop_Address : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Workshops");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_BuildingNumber",
+                table: "Workshops",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "Workshops",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Region",
+                table: "Workshops",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Street",
+                table: "Workshops",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_BuildingNumber",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Region",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Street",
+                table: "Workshops");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Workshops",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
