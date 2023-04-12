64b56a20952477af86424e763fb14b3e95f0bc27
strivitech <strivitech@gmail.com>
2023-04-12 12:35:09 +0300

    Add migration updating workshop address region

---
+++ WorkshopSearch/WebApp/Database/Main/Migrations/20230412093019_Update_Worskhop_Address_Region.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Main.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Worskhop_Address_Region : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address_Region",
+                table: "Workshops",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address_Region",
+                table: "Workshops",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
