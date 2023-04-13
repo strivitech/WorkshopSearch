b57d4c10354030f85ffaf19df2d8835aa1a3992c
strivitech <strivitech@gmail.com>
2023-04-13 15:50:26 +0300

    Add errors messages

---
+++ WorkshopSearch/WebApp/Common/Application/Errors.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+
+namespace WebApp.Common.Application;
+
+public static partial class Errors
+{
+    public static class DecisionMakingAnalysis
+    {
+        public static Error OrderAnalysisUnexpected => Error.Unexpected("DecisionMakingAnalysis.OrderAnalysisUnexpected",
+            "Unexpected error while ordering analysis models");
+    }
+}
