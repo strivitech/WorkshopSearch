0687cf60ce0d0d08aca524ebbe633f5153e3e056
strivitech <strivitech@gmail.com>
2023-04-13 18:27:36 +0300

    Update errors for workshops

--- WorkshopSearch/WebApp/Features/Workshops/Errors.cs
+++ WorkshopSearch/WebApp/Features/Workshops/Errors.cs
@@ -6,15 +6,13 @@ public static partial class Errors
 {
     public static class Workshops
     {
-        public static Error DecisionMakingAnalysisOrderingFailed => Error.Failure(
-            "Workshops.DecisionMakingAnalysisOrderingFailed",
-            "Ordering workshop analysis models failed");
-
         public static Error GetByFilterFailure => Error.Failure("Workshops.GetByFilterFailure",
             "Getting workshops by filter failed");
 
         public static Error GetByDecisionMakingAnalysisFailure =>
             Error.Failure("Workshops.GetByDecisionMakingAnalysisFailure",
-                "Getting workshops by decision making analysis failed"); 
+                "Getting workshops by decision making analysis failed");
+        
+        public static Error NotFound => Error.Failure("Workshops.NotFound", "Workshop not found");
     }
 }
