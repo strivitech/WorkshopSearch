562d0228305aa663f60709004c705f1fc14272e4
strivitech <strivitech@gmail.com>
2023-04-13 18:28:08 +0300

    Add getting by id for workshops

--- WorkshopSearch/WebApp/Features/Workshops/IWorkshopService.cs
+++ WorkshopSearch/WebApp/Features/Workshops/IWorkshopService.cs
@@ -4,6 +4,8 @@ namespace WebApp.Features.Workshops;
 
 public interface IWorkshopService
 {
+    Task<ErrorOr<WorkshopResponse>> GetByIdAsync(Guid id);
+
     Task<ErrorOr<List<ShortWorkshopResponse>>> GetByFilterAsync(WorkshopFilter filter);
     
     Task<ErrorOr<List<ShortWorkshopResponse>>> GetByDecisionMakingAnalysisAsync(WorkshopFilter filter);
