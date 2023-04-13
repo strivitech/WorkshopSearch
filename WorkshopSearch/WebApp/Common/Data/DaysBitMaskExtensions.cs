fe338b394eded43fc9d504b6c86db933ade76a17
strivitech <strivitech@gmail.com>
2023-04-13 18:28:59 +0300

    Add mapping Workshop -> WorkshopResponse, Direction -> DirectionDto, DaysBitMask -> List of strings

---
+++ WorkshopSearch/WebApp/Common/Data/DaysBitMaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Common.Data;
+
+public static class DaysBitMaskExtensions
+{
+    public static List<string> GetDaysFromBitMask(this DaysBitMask daysBitMask)
+    {
+        var days = Enum.GetValues(typeof(DaysBitMask))
+            .Cast<DaysBitMask>()
+            .Where(d => daysBitMask.HasFlag(d) && d != 0)
+            .Select(d => d.ToString())
+            .ToList();
+
+        return days;
+    }
+}
