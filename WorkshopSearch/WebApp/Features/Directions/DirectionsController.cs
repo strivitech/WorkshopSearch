7fbef3f3b69777ce2dacbae742595d7f5ca7bba1
strivitech <111649007+strivitech@users.noreply.github.com>
2023-04-18 22:41:57 +0300

    Add directions data to backend (#24)

---
+++ WorkshopSearch/WebApp/Features/Directions/DirectionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Common.API;
+
+namespace WebApp.Features.Directions;
+
+[ApiController]
+[Route(ControllersRouteConstants.DefaultRestRoute)]
+public class DirectionsController : ControllerBase
+{
+    private readonly IDirectionsService _directionsService;
+
+    public DirectionsController(IDirectionsService directionsService)
+    {
+        _directionsService = directionsService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var directions = await _directionsService.GetAllAsync();
+        return directions.Any()
+            ? Ok(directions)
+            : NoContent();
+    }
+}
---
+++ WorkshopSearch/WebApp/Features/Directions/DirectionsService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Database.Main;
+
+namespace WebApp.Features.Directions;
+
+public class DirectionsService : IDirectionsService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public DirectionsService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<List<DirectionDto>> GetAllAsync()
+    {
+        var directions = await _dbContext.Directions
+            .Select(direction => new DirectionDto
+            {
+                Id = direction.Id.Value,
+                Name = direction.Name
+            })
+            .ToListAsync();
+        
+        return directions;
+    }
+}
