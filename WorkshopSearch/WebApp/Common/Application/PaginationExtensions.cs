1732020f2f9909fc23a00d46e89ec45992c4288a
strivitech <strivitech@gmail.com>
2023-04-21 20:26:34 +0300

    Add pagination extensions

---
+++ WorkshopSearch/WebApp/Common/Application/PaginationExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Common.Application;
+
+public static class PaginationExtensions
+{
+    public static int CalculateTotalPages(this int totalItems, int pageSize)
+    {
+        return (int) Math.Ceiling(totalItems / (double) pageSize);
+    }
+}
