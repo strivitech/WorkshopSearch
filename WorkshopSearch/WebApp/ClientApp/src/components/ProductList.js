b678cda817b0fcd1577345b1a8cd3d375e44733a
strivitech <strivitech@gmail.com>
2023-04-21 21:02:04 +0300

    If not products then show const text

--- WorkshopSearch/WebApp/ClientApp/src/components/ProductList.js
+++ WorkshopSearch/WebApp/ClientApp/src/components/ProductList.js
@@ -5,11 +5,7 @@ import { useProductsContext } from '../context/products_context';
 const ProductList = () => {
   const { products } = useProductsContext();
 
-  if (!products) {
-    return <h4>Loading...</h4>;
-  }
-
-  if (products.length < 1) {
+  if (!products || products.length < 1) {
     return (
         <h5 style={{ textTransform: 'none' }}>
           Вибачте, жодного гуртка не знайдено за вашим запитом :(
