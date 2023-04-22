2e5c273ff9c9061456a3287d1029205ff64f65fa
strivitech <strivitech@gmail.com>
2023-04-22 12:32:49 +0300

    Reset page to first when filter is updated

--- WorkshopSearch/WebApp/ClientApp/src/context/products_context.js
+++ WorkshopSearch/WebApp/ClientApp/src/context/products_context.js
@@ -1,132 +1,144 @@
-import axios from 'axios'
-import React, {useContext, useEffect, useReducer, useState} from 'react'
-import reducer from '../reducers/products_reducer'
+import axios from 'axios';
+import React, {useContext, useEffect, useReducer, useState} from 'react';
+import reducer from '../reducers/products_reducer';
 import {
-  SIDEBAR_OPEN,
-  SIDEBAR_CLOSE,
-  GET_PRODUCTS_BEGIN,
-  GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR,
-  GET_SINGLE_PRODUCT_BEGIN,
-  GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
-import {useFilterContext} from "./filter_context";
+    SIDEBAR_OPEN,
+    SIDEBAR_CLOSE,
+    GET_PRODUCTS_BEGIN,
+    GET_PRODUCTS_SUCCESS,
+    GET_PRODUCTS_ERROR,
+    GET_SINGLE_PRODUCT_BEGIN,
+    GET_SINGLE_PRODUCT_SUCCESS,
+    GET_SINGLE_PRODUCT_ERROR,
+} from '../actions';
+import {useFilterContext} from './filter_context';
 
 const initialState = {
-  isSidebarOpen: false,
-  products_loading: false,
-  products_error: false,
-  products: [],
-  single_product_loading: false,
-  single_product_error: false,
-  single_product: {},
-  totalPages: 0, 
-    currentPage: 1
-}
-
-const DEFAULT_FILTERS = {
-  text: '',
-  category: 1,
-  minAge: 0,
-  maxAge: 100,
-  minPrice: 0,
-  maxPrice: 100000,
-  workingDays: {
-    monday: false,
-    tuesday: false,
-    wednesday: false,
-    thursday: false,
-    friday: false,
-    saturday: false,
-    sunday: false,
-  },
-  regionWithCity: 'Київ,Київ'
+    isSidebarOpen: false,
+    products_loading: false,
+    products_error: false,
+    products: [],
+    single_product_loading: false,
+    single_product_error: false,
+    single_product: {},
+    totalPages: 0,
+    currentPage: 1,
 };
 
-const ProductsContext = React.createContext()
+const DEFAULT_FILTERS = {
+    text: '',
+    category: 1,
+    minAge: 0,
+    maxAge: 100,
+    minPrice: 0,
+    maxPrice: 100000,
+    workingDays: {
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false,
+        saturday: false,
+        sunday: false,
+    },
+    regionWithCity: 'Київ,Київ',
+};
+
+const ProductsContext = React.createContext();
 
 // TODO: Add filter validation
-export const ProductsProvider = ({ children }) => {
-  const DEBOUNCE_DELAY = 2000;
+export const ProductsProvider = ({children}) => {
+    const DEBOUNCE_DELAY = 2000;
 
-  function useDebounce(value, delay) {
-    const [debouncedValue, setDebouncedValue] = useState(value);
+    function useDebounce(value, delay) {
+        const [debouncedValue, setDebouncedValue] = useState(value);
+
+        useEffect(() => {
+            const timerId = setTimeout(() => {
+                setDebouncedValue(value);
+            }, delay);
+
+            return () => {
+                clearTimeout(timerId);
+            };
+        }, [value, delay]);
+
+        return debouncedValue;
+    }
+
+    const {filters} = useFilterContext();
+
+    const [currentPage, setCurrentPage] = useState(1, (page) => {
+        fetchData(page);
+    });
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const debouncedFilters = useDebounce(filters, DEBOUNCE_DELAY);
 
     useEffect(() => {
-      const timerId = setTimeout(() => {
-        setDebouncedValue(value);
-      }, delay);
+        setCurrentPage(1);
+    }, [debouncedFilters]);
 
-      return () => {
-        clearTimeout(timerId);
-      };
-    }, [value, delay]);
+    useEffect(() => {
+        fetchData(currentPage);
+    }, [currentPage]);
 
-    return debouncedValue;
-  }
+    const fetchData = async (page) => {
+        let filtersToUri = {};
 
-  const { filters } = useFilterContext()
+        const [region, city] = filters.regionWithCity.split(',');
+        filtersToUri['regionWithCity.region'] = region;
+        filtersToUri['regionWithCity.city'] = city;
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const debouncedFilters = useDebounce(filters, DEBOUNCE_DELAY);
-  useEffect(() => {
-    let filtersToUri = {}
+        filtersToUri.categoryId = filters.category;
 
-    const [region, city] = filters.regionWithCity.split(',');
-    filtersToUri["regionWithCity.region"] = region;
-    filtersToUri["regionWithCity.city"] = city;
+        if (filters.text !== DEFAULT_FILTERS.text) {
+            filtersToUri.text = filters.text;
+        }
 
-    filtersToUri.categoryId = filters.category;
+        if (filters.minAge !== DEFAULT_FILTERS.minAge) {
+            filtersToUri.minAge = filters.minAge;
+        }
 
-    if (filters.text !== DEFAULT_FILTERS.text) {
-      filtersToUri.text = filters.text;
-    }
+        if (filters.maxAge !== DEFAULT_FILTERS.maxAge) {
+            filtersToUri.maxAge = filters.maxAge;
+        }
 
-    if (filters.minAge !== DEFAULT_FILTERS.minAge) {
-      filtersToUri.minAge = filters.minAge;
-    }
+        if (filters.minPrice !== DEFAULT_FILTERS.minPrice) {
+            filtersToUri.minPrice = filters.minPrice;
+        }
 
-    if (filters.maxAge !== DEFAULT_FILTERS.maxAge) {
-      filtersToUri.maxAge = filters.maxAge;
-    }
+        if (filters.maxPrice !== DEFAULT_FILTERS.maxPrice) {
+            filtersToUri.maxPrice = filters.maxPrice;
+        }
 
-    if (filters.minPrice !== DEFAULT_FILTERS.minPrice) {
-      filtersToUri.minPrice = filters.minPrice;
-    }
+        const selectedDays = Object.keys(filters.workingDays).filter(
+            (key) => filters.workingDays[key],
+        );
+        if (selectedDays.length > 0) {
+            filtersToUri.workshop_days = selectedDays.join(',');
+        }
 
-    if (filters.maxPrice !== DEFAULT_FILTERS.maxPrice) {
-      filtersToUri.maxPrice = filters.maxPrice;
-    }
-
-    const selectedDays = Object.keys(filters.workingDays).filter(key => filters.workingDays[key]);
-    if (selectedDays.length > 0) {
-      filtersToUri.workshop_days = selectedDays.join(',');
-    }
-
-    let urlParams = new URLSearchParams(filtersToUri).toString();
-    const from = currentPage;
-    const size = 2;
-    if (urlParams.length > 0) {
-      fetchProducts(`api/workshops` + '?' + urlParams, from, size);
-    } else {
-      fetchProducts(`api/workshops`, from, size);
-    }
-
-  }, [debouncedFilters, currentPage])
+        let urlParams = new URLSearchParams(filtersToUri).toString();
+        const from = page;
+        const size = 9;
+        if (urlParams.length > 0) {
+            fetchProducts(`api/workshops` + '?' + urlParams, from, size);
+        } else {
+            fetchProducts(`api/workshops`, from, size);
+        }
+    };
 
     const openSidebar = () => {
-        dispatch({ type: SIDEBAR_OPEN })
-    }
+        dispatch({ type: SIDEBAR_OPEN });
+    };
     const closeSidebar = () => {
-        dispatch({ type: SIDEBAR_CLOSE })
-    }
-    
+        dispatch({ type: SIDEBAR_CLOSE });
+    };
+
     const changePage = (newPage) => {
         setCurrentPage(newPage);
     };
-  
+
     const fetchProducts = async (url, from, size) => {
         dispatch({ type: GET_PRODUCTS_BEGIN });
         try {
@@ -139,15 +151,15 @@ export const ProductsProvider = ({ children }) => {
     };
 
     const fetchSingleProduct = async (url) => {
-        dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+        dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
         try {
-            const response = await axios.get(url)
-            const singleProduct = response.data
-            dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
+            const response = await axios.get(url);
+            const singleProduct = response.data;
+            dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
         } catch (error) {
-            dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+            dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
         }
-    }
+    };
 
     return (
         <ProductsContext.Provider
@@ -157,14 +169,15 @@ export const ProductsProvider = ({ children }) => {
                 closeSidebar,
                 fetchSingleProduct,
                 changePage,
-                currentPage
+                currentPage,
             }}
         >
             {children}
         </ProductsContext.Provider>
-    )
-}
+    );
+};
 
 export const useProductsContext = () => {
-    return useContext(ProductsContext)
-}
+    return useContext(ProductsContext);
+};
+
