5b77f8a6259abf185bb722b0f4e07aff720dc69a
strivitech <strivitech@gmail.com>
2023-04-23 23:18:04 +0300

    Add Location model

---
+++ WorkshopSearch/WebApp/Features/Locations/Location.cs
@@ -0,0 +1,13 @@
+﻿using Nest;
+
+namespace WebApp.Features.Locations;
+
+public class Location
+{
+    [Keyword]
+    public string Id { get; set; } = null!;
+
+    [Text(Analyzer = "ukrainian_analyzer")]
+    public string LocationName { get; set; } = null!;
+}
+
