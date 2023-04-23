ea6683d745e10a4670200bd7d91796fa16215562
strivitech <strivitech@gmail.com>
2023-04-23 23:19:17 +0300

    Add hosted service to create locations index

---
+++ WorkshopSearch/WebApp/Features/Locations/ElasticsearchIndexHostedService.cs
@@ -0,0 +1,33 @@
+﻿using WebApp.Elasticsearch;
+
+namespace WebApp.Features.Locations;
+
+public class ElasticsearchIndexHostedService : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ElasticsearchIndexHostedService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var indexInitializer = scope.ServiceProvider.GetRequiredService<ILocationIndexInitializer>();
+
+        try
+        {
+            await indexInitializer.InitializeAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("Failed to create Elasticsearch index during startup.", ex);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
