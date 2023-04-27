79441ba53738087c532396ccdbfa26d388c89af6
strivitech <strivitech@gmail.com>
2023-04-27 20:11:56 +0300

    Add locations controller

---
+++ WorkshopSearch/WebApp/Features/Locations/LocationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Common.API;
+
+namespace WebApp.Features.Locations;
+
+[ApiController]
+[Route(ControllersRouteConstants.DefaultRestRoute)]
+public class LocationsController : ControllerBase
+{
+    private readonly ILocationsService _locationsService;
+
+    public LocationsController(ILocationsService locationsService)
+    {
+        _locationsService = locationsService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetByWord([FromQuery] string word)
+    {
+        var locations = await _locationsService.GetBySearchTermAsync(word);
+        return locations.Any()
+            ? Ok(locations)
+            : NoContent();
+    }
+}
