805d348d682609d1f4bb9b76210e81f43a2301d1
strivitech <strivitech@gmail.com>
2023-04-27 20:11:46 +0300

    Add location service

---
+++ WorkshopSearch/WebApp/Features/Locations/LocationsService.cs
@@ -0,0 +1,36 @@
+﻿using WebApp.Elasticsearch;
+
+namespace WebApp.Features.Locations;
+
+public class LocationsService : ILocationsService
+{
+    private const int GetBySearchTermMaxSize = 25;
+    private readonly IElasticsearchService _elasticsearchService;
+
+    public LocationsService(IElasticsearchService elasticsearchService)
+    {
+        _elasticsearchService = elasticsearchService;
+    }
+    
+    public async Task<List<LocationEsModel>> GetBySearchTermAsync(string searchTerm)
+    {
+        var client = _elasticsearchService.GetClient();
+        var searchResponse = await client.SearchAsync<LocationEsModel>(s => s
+            .Index(IndexNames.Locations)
+            .Size(GetBySearchTermMaxSize)
+            .Query(q => q
+                .MatchPhrasePrefix(m => m
+                    .Field(f => f.Name)
+                    .Query(searchTerm)
+                )
+            )
+        );
+
+        if (!searchResponse.IsValid)
+        {
+            throw new InvalidOperationException("Failed to search locations.");
+        }
+
+        return searchResponse.Documents.ToList();
+    }
+}
